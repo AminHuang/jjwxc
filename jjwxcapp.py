d6ec94e70ae437893e3cfec898c2b71c704bbcc2
Amin <skuare520@gmail.com>
2015-02-18 14:10:31 +0800

    Update jjwxcapp.py

--- jjwxcapp.py
+++ jjwxcapp.py
@@ -33,8 +33,8 @@ headers = {
                'User-Agent'      : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36'
              }
 
-username='kom9ing@163.com'
-password='3310571'
+username='your - username'
+password='your - password'
 
 if __name__=='__main__':
     # username = raw_input('username:')
